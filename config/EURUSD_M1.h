e6138416cfec078eed913b7fe1a7b3fee79a10f4
kenorb <kenorb@users.noreply.github.com>
2021-01-17 04:36:47 +0000

    Optimize Indicator params (pf=0.27; dd=21.30; tt=20825)

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -1,4 +1,4 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
@@ -7,9 +7,9 @@
 struct Indi_OsMA_Params_M1 : OsMAParams {
   Indi_OsMA_Params_M1() : OsMAParams(indi_osma_defaults, PERIOD_M1) {
     applied_price = (ENUM_APPLIED_PRICE)0;
-    ema_fast_period = 10;
-    ema_slow_period = 54;
-    signal_period = 14;
+    ema_fast_period = 2;
+    ema_slow_period = 66;
+    signal_period = 22;
     shift = 0;
   }
 } indi_osma_m1;
@@ -21,12 +21,12 @@ struct Stg_OsMA_Params_M1 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 0;
     signal_close_method = 0;
     signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
